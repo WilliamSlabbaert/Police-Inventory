ae6a40d94d36468117e9eb75e48dc2a1f850e28b
William Slabbaert <56199868+WilliamSlabbaert@users.noreply.github.com>
2024-02-20 17:48:30 +0100

    serial number

--- Police System/models/Employee.cs
+++ Police System/models/Employee.cs
@@ -15,7 +15,7 @@ namespace Police_System.models
 
         public Address Address { get; set; }
         public string BadgeNumber { get; set; }
-        public string Role {  get; set; }
+        public string Role { get; set; }
 
         public Weapon CurrentWeapon { get; set; }
         public Vehicle CurrentVehicle { get; set; }
--- Police System/models/Weapon.cs
+++ Police System/models/Weapon.cs
@@ -11,7 +11,7 @@ namespace Police_System.models
         public string Brand { get; set; }
         public string Model { get; set; }
         public string Color { get; set; }
-
+        public string SerialNumber { get; set; }
         public DateTime yearOfManufacture { get; set; }
         public string AmmoType { get; set; }
         public int MagazineClipSize { get; set; }
